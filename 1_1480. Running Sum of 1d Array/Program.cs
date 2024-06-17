9968cf28f5c665e4976c3de1926d30c0156fff5f
tmarcorf <tmarco783@gmail.com>
2024-06-16 23:15:56 -0300

    feat: adicionando exercício 1480

---
+++ 1_1480. Running Sum of 1d Array/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _1_1480._Running_Sum_of_1d_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Read();
+        }
+
+        public int[] RunningSum(int[] nums)
+        {
+            int[] arrayResult = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                arrayResult[i] = SumUntilPosition(nums, i);
+            }
+
+            return arrayResult;
+        }
+
+        private int SumUntilPosition(int[] nums, int position)
+        {
+            int sumResult = 0;
+
+            for (int i = 0; i <= position; i++)
+            {
+                sumResult += nums[i];
+            }
+
+            return sumResult;
+        }
+    }
+}
