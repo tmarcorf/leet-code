f481e4c49a7375f549ec92d97cfb8310a7a3d12a
tmarcorf <tmarco783@gmail.com>
2024-06-22 20:55:43 -0300

    feat: adicionando exercício 412

---
+++ solutions/3_412. Fizz Buzz/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+public IList<string> FizzBuzz(int n) 
+{
+    string[] answer = new string[n];
+
+
+    for (int i = 1; i <= n; i++)
+    {
+        bool divisibleByThree = i % 3 == 0;
+        bool divisibleByFive = i % 5 == 0;
+
+        if (divisibleByThree && divisibleByFive) 
+        {
+            answer[i - 1] = "FizzBuzz";
+        }
+        else if (divisibleByThree)
+        {
+            answer[i - 1] = "Fizz";
+        }
+        else if (divisibleByFive)
+        {
+            answer[i - 1] = "Buzz";
+        }
+        else
+        {
+            answer[i - 1] = i.ToString();
+        }
+    }        
+
+    return answer;
+}
