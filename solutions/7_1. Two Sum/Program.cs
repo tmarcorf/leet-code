8b8b288d59b05d1227f76d5e7b65d7109b80ca32
tmarcorf <tmarco783@gmail.com>
2024-06-23 18:59:42 -0300

    refactor: diminuindo linhas de código do exercício 7

--- solutions/7_1. Two Sum/Program.cs
+++ solutions/7_1. Two Sum/Program.cs
@@ -1,19 +1,14 @@
 ﻿public int[] TwoSum(int[] nums, int target) {
-    int[] returnArray = new int[2];
-
     for (int i = 0; i < nums.Length; i++) 
     {
         for (int j = 0; j < nums.Length; j++)
         {
             if (i != j && (nums[i] + nums[j]) == target)
             {
-                returnArray[0] = i;
-                returnArray[1] = j;
+                return new int[] {i, j};
             }
         }
     }
 
-    return returnArray;
+    return null;
 }
-
-public bool 
