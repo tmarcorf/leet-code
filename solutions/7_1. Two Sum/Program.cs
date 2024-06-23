5ca77450f4d8df08ccaca6feac56729aa40d6b28
tmarcorf <tmarco783@gmail.com>
2024-06-23 18:55:05 -0300

    feat: adicionando exercício 1

---
+++ solutions/7_1. Two Sum/Program.cs
@@ -0,0 +1,19 @@
+﻿public int[] TwoSum(int[] nums, int target) {
+    int[] returnArray = new int[2];
+
+    for (int i = 0; i < nums.Length; i++) 
+    {
+        for (int j = 0; j < nums.Length; j++)
+        {
+            if (i != j && (nums[i] + nums[j]) == target)
+            {
+                returnArray[0] = i;
+                returnArray[1] = j;
+            }
+        }
+    }
+
+    return returnArray;
+}
+
+public bool 
